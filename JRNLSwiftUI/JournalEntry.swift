c5914be6a4a96e2262bf15f500f64b6d8cdd096b
DopeKS <kyusang.dev@gmail.com>
2024-05-28 15:30:11 +0900

    JournalDetailView 생성

--- JRNLSwiftUI/JournalEntry.swift
+++ JRNLSwiftUI/JournalEntry.swift
@@ -12,6 +12,7 @@ struct JournalEntry: Identifiable, Hashable {
     let id = UUID()
     let date = Date()
     let entryTitle: String
+    let entryBody: String
     let photo: UIImage?
     let latitude: Double?
     let longitude: Double?
@@ -19,10 +20,10 @@ struct JournalEntry: Identifiable, Hashable {
 
 //MARK: - Sample data
 let testData = [
-    JournalEntry(entryTitle: "날씨 죽인다 행배야", photo: UIImage(systemName: "sun.max"), latitude: 37.3346, longitude: -122.0090),
-    JournalEntry(entryTitle: "날씨 죽인다 행배야", photo: UIImage(systemName: "sun.snow"), latitude: 37.3346, longitude: -122.0090),
-    JournalEntry(entryTitle: "날씨 죽인다 행배야", photo: UIImage(systemName: "sunset"), latitude: 37.3346, longitude: -122.0090),
-    JournalEntry(entryTitle: "날씨 죽인다 행배야", photo: UIImage(systemName: "snowflake"), latitude: 37.3346, longitude: -122.0090)
+    JournalEntry(entryTitle: "날씨 죽인다 행배야", entryBody: "마 이 개이새꺄 마 다 했어!", photo: UIImage(systemName: "sun.max"), latitude: 37.3346, longitude: -122.0090),
+    JournalEntry(entryTitle: "날씨 죽인다 행배야", entryBody: "이새끼들이 말이야", photo: UIImage(systemName: "sun.snow"), latitude: 37.3346, longitude: -122.0090),
+    JournalEntry(entryTitle: "날씨 죽인다 행배야", entryBody: "개새끼들", photo: UIImage(systemName: "sunset"), latitude: 37.3346, longitude: -122.0090),
+    JournalEntry(entryTitle: "날씨 죽인다 행배야", entryBody: "실례지만 어데 최씹니까?", photo: UIImage(systemName: "snowflake"), latitude: 37.3346, longitude: -122.0090)
 ]
 
 
---
+++ JRNLSwiftUI/JournalEntryDetailView.swift
@@ -0,0 +1,62 @@
+//
+//  JournalEntryDetailView.swift
+//  JRNLSwiftUI
+//
+//  Created by 황규상 on 5/28/24.
+//
+
+import SwiftUI
+struct JournalEntryDetail: View {
+    var selectedJournalEntry: JournalEntry
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Spacer()
+                    .frame(height: 30)
+                
+                Text(selectedJournalEntry.date.formatted(.dateTime.day().month().year()))
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                
+                Spacer()
+                    .frame(height: 30)
+                
+                Text(selectedJournalEntry.entryTitle)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                    .frame(height: 30)
+                
+                Text(selectedJournalEntry.entryBody)
+                    .font(.title2)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                    .frame(height: 30)
+                
+                Image(uiImage: selectedJournalEntry.photo ?? UIImage(systemName: "face.smiling")!)
+                    .resizable()
+                    .frame(width: 300, height: 300)
+                
+                Spacer()
+                    .frame(height: 30)
+                
+                MapView(journalEntry: selectedJournalEntry)
+                    .frame(width: 300, height: 300)
+                
+            }
+            .padding()
+            .navigationTitle("Entry Detail")
+        }
+    }
+}
+
+#Preview {
+    NavigationView {
+        JournalEntryDetail(selectedJournalEntry: testData[0])
+    }
+}
