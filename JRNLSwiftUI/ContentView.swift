9516d32f14a12e6c455e1291b77d6f59ebfd60d4
DopeKS <kyusang.dev@gmail.com>
2024-05-28 14:49:09 +0900

    Journal List View 수정 및 모델 객체 구성

--- JRNLSwiftUI/ContentView.swift
+++ JRNLSwiftUI/ContentView.swift
@@ -9,23 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            Image(systemName: "face.smiling")
-                .resizable()
-                .frame(width: 90, height: 90)
+        List(0 ..< 5) { item in
             VStack {
-                Text("2024.05.28")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                Text("날씨 죽인다 행배야")
-                    .font(.title2)
-                    .foregroundStyle(.secondary)
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                HStack {
+                    Image(systemName: "face.smiling")
+                        .resizable()
+                        .frame(width: 90, height: 90)
+                    VStack {
+                        Text("2024.05.28")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Text("날씨 죽인다 행배야")
+                            .font(.title2)
+                            .foregroundStyle(.secondary)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    .padding()
+                }
             }
-            .padding()
         }
-        .padding()
     }
 }
 
