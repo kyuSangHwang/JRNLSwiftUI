fc6f766977a92b2a08c0b84e31cadb2e15e92aa7
DopeKS <kyusang.dev@gmail.com>
2024-05-28 14:31:15 +0900

    Initial Commit

---
+++ JRNLSwiftUI/JRNLSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JRNLSwiftUIApp.swift
+//  JRNLSwiftUI
+//
+//  Created by 황규상 on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct JRNLSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
